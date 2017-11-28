6b2007b002641dd732da0faa42bf5ab412812d08
Zeluli <lizelusdut@qq.com>
2017-11-28 14:42:55 +0800

    Update Replace Condition with Polymorphism(重构后).swift

--- 代码重构/条件表达式重构/以多态取代条件表达式/Replace Condition with Polymorphism(重构后).swift
+++ 代码重构/条件表达式重构/以多态取代条件表达式/Replace Condition with Polymorphism(重构后).swift
@@ -1,5 +1,4 @@
 //使用多态进行重构
-
 protocol Price {
     func charge() -> Double
 }
@@ -26,35 +25,35 @@ class Book {
     static let NEW_BOOK = 0
     static let OLD_BOOK = 1
     static let CHIDREN_BOOK = 2
-
+    
     var bookCode:Int = 0
     var bookName:String
     var price: Price? = nil
-
+    
     init(bookCode:Int, bookName:String) {
         self.bookName = bookName
-        self.setBookCode(bookCode)
+        self.setBookCode(bookCode: bookCode)
     }
     
     private func setBookCode(bookCode: Int) {
         self.bookCode = bookCode
         switch self.bookCode {
-            case Book.NEW_BOOK:
-                self.price = NewBookPrice()
-            case Book.OLD_BOOK:
-                self.price = OldBookPrice()
-            case Book.CHIDREN_BOOK:
-                self.price = ChildrenBookPrice()
-            default:
-                break;
+        case Book.NEW_BOOK:
+            self.price = NewBookPrice()
+        case Book.OLD_BOOK:
+            self.price = OldBookPrice()
+        case Book.CHIDREN_BOOK:
+            self.price = ChildrenBookPrice()
+        default:
+            break;
         }
-
+        
     }
-
+    
     func charge() -> Double {
         return self.price!.charge()
     }
-
+    
 }
 
 //let book = Book(bookCode: Book.NEW_BOOK, bookName: "《福尔摩斯》")
